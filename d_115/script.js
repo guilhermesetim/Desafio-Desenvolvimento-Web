822fcb97658ee2013fbb3ef47ed292d80079c666
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2021-11-10 15:15:51 -0300

    d115 - coleta do valor da opção
    coleta do valor do inptu radio no formulário.

--- d_115/script.js
+++ d_115/script.js
@@ -1,4 +1,6 @@
 document.querySelector('#buttonExecut').addEventListener('submit',function(event){
     event.preventDefault()
-    alert('teste')
+    
+    let valueOption = document.querySelector('input[name="optionSelect"]:checked').value
+    alert(valueOption);
 })

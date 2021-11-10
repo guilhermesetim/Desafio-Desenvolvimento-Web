b5e839483239e144aa99e923d632665e694816a4
Guilherme Setim <64696390+guilhermesetim@users.noreply.github.com>
2021-11-10 16:06:07 -0300

    d115 function options
    Declarações das funções permitidas pelo input radio

--- d_115/script.js
+++ d_115/script.js
@@ -2,5 +2,42 @@ document.querySelector('#buttonExecut').addEventListener('submit',function(event
     event.preventDefault()
     
     let valueOption = document.querySelector('input[name="optionSelect"]:checked').value
-    alert(valueOption);
+    
+    checkOptions(valueOption);
+
 })
+
+
+
+function showDataBase(){
+    alert('Mostrar Usuários')
+}
+
+
+function newCadastre(){
+    alert('Cadastrar')
+}
+
+function exit(){
+    alert('Programa finalizado')
+}
+
+
+
+function checkOptions(option){
+    switch(option){
+        case 'showUsr':
+            showDataBase()
+            break
+        
+        case 'newUsr':
+            newCadastre()
+            break
+
+        case 'exit':
+            exit()
+            break
+        default:
+            alert('Opção inválida!')
+    }
+}
